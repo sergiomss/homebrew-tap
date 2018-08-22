1a7a4e0db144c13f84f4c123a3e92481f5025ad5
Sérgio Silva <16949578+majestic-fox@users.noreply.github.com>
2018-08-22 13:38:59 +0200

    Updated swiss-army-knife formula's sha

--- swiss-army-knife.rb
+++ swiss-army-knife.rb
@@ -3,7 +3,7 @@ class SwissArmyKnife < Formula
   homepage "https://github.com/majestic-fox/swiss-army-knife"
   url "https://github.com/majestic-fox/swiss-army-knife/releases/download/0.0.1/swiss-army-knife_darwin_amd64"
   version "0.0.1"
-  sha256 "e3a3f22d34db13c1ff1510caee7198e25cf6b575e4f0338d080545d022ac323f"
+  sha256 "648d939ac59da412c84288c16dff2155a4a6512d252b8fece67fa9c5f128b4fb"
   # depends_on "cmake" => :build
 
   def install
