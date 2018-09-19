acf74d6c01b10f4576d08e78ea8187e07eb020f7
Sérgio Silva <16949578+majestic-fox@users.noreply.github.com>
2018-09-19 16:56:14 +1000

    Update swiss-army-knife.rb

--- swiss-army-knife.rb
+++ swiss-army-knife.rb
@@ -1,9 +1,9 @@
 class SwissArmyKnife < Formula
   desc "A collection of go packages in one command line tool"
   homepage "https://github.com/majestic-fox/swiss-army-knife"
-  url "https://github.com/majestic-fox/swiss-army-knife/releases/download/0.0.1/swiss-army-knife_darwin_amd64"
+  url "https://github.com/majestic-fox/swiss-army-knife/releases/download/0.0.2/swiss-army-knife_darwin_amd64"
   version "0.0.1"
-  sha256 "648d939ac59da412c84288c16dff2155a4a6512d252b8fece67fa9c5f128b4fb"
+  sha256 "ec59b8fd7b1c4e54d949336b7dd2686d2ff9bdd2404a7bde5182360cab389d95"
   # depends_on "cmake" => :build
 
   def install
