bf1408dd5686621eb823c8d66d556e477e3ef971
Sérgio Silva <16949578+sergiomss@users.noreply.github.com>
2019-05-11 21:39:46 +0200

    Update ks.rb

--- ks.rb
+++ ks.rb
@@ -1,7 +1,7 @@
 class Ks < Formula
   desc "Switch between kube contexts faster"
-  homepage "https://github.com/majestic-fox/ks"
-  url "https://github.com/majestic-fox/ks/releases/download/0.0.9/ks_darwin_amd64"
+  homepage "https://github.com/sergiomss/ks"
+  url "https://github.com/sergiomss/ks/releases/download/0.0.9/ks_darwin_amd64"
   version "0.0.9"
   sha256 "ef96061192f65d294b7062861f8a4453fb0bd57a925fb3bd5b7e0161c4e61ca4"
   # depends_on "cmake" => :build
