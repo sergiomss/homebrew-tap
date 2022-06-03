2b05fb7796921423b5a94aca9ccc58d0ffcd67de
sergio <16949578+sergiomss@users.noreply.github.com>
2022-06-03 16:35:39 +0200

    Update ks.rb

--- ks.rb
+++ ks.rb
@@ -1,9 +1,9 @@
 class Ks < Formula
   desc "Switch between kube contexts faster"
   homepage "https://github.com/sergiomss/ks"
-  url "https://github.com/sergiomss/ks/releases/download/0.2.0/ks_darwin_amd64"
+  url "https://github.com/sergiomss/ks/releases/download/1.0.0/ks_darwin_amd64"
   version "0.2.0"
-  sha256 "720fe6a388a8a4e0f1d1b77717968a2bbf1bc5903067f73f7e475598e8c538ce"
+  sha256 "c71f33ec28d8f55681a5a503e40ac697d64395b6045b9c8d22c2f757a1eb1c79"
   # depends_on "cmake" => :build
 
   def install
